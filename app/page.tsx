1f609b8e3698d8fbbeedc5407e8ffda613f3493b
Jules White <juleswhite@127.0.0.1>
2024-04-25 23:00:22 +0100

    Update to proper protocol spec

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,34 @@
+"use client"
+
+import { useEffect, useState } from 'react';
 import * as Packet from '../lib/packet'
 
 export default function Home() {
-    const packet = Packet.NewPacket(11, 200, 0, "Test");
+    const [packetData, setPacketData] = useState<string | null>('test');
 
-    (async () => {
-        try {
-            const data: Uint8Array = await Packet.SendPacket(packet);
-            console.log(data.length);
-            data.forEach((x, i) => {
-                console.log(`Byte ${i}: ${x}`);
-            });
-            const recv: Packet.Packet = Packet.FromBytes(data);
-            console.log("Received Version:", recv.version);
-            console.log("Received Command:", recv.command);
-            console.log("Received Status:", recv.status);
-            console.log("Received Id:", recv.id);
-            console.log("Received Data:", recv.data);
-        }
-        catch (error) {
-            console.error('error receiving error: ', error);
-        }
-    })();
+    console.log("Hey!1");
+
+    useEffect(() => {
+        console.log("Hey!2");
+        const fetchData = async () => {
+            console.log("Hey!3");
+            try {
+                const packet = Packet.NewPacket(11, 200, 0, "Test");
+                const data: Uint8Array = await Packet.SendPacket(packet);
+                const recv: Packet.Packet = Packet.FromBytes(data);
+                setPacketData(recv.data);
+            }
+            catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    });
 
     return (
         <div>
+            {packetData ? packetData : "No Data"}
         </div>
     );
 }
